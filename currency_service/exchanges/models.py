6222e5c533eb87517a5c3636d47f6e4fcc42002c
Artuom4ik <masterskayaigr123@gmail.com>
2024-05-27 22:43:39 +0300

    Rewrite ExchangeRate model

--- currency_service/exchanges/models.py
+++ currency_service/exchanges/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class ExchangeRate(models.Model):
-    date = models.DateField()
-    currency = models.CharField(max_length=10)
-    rate = models.DecimalField(max_digits=10, decimal_places=4)
+    date = models.DateField(verbose_name="Дата курса")
+    currency_id = models.IntegerField(max_length=10, verbose_name="ID валюты")
+    currency_name = models.CharField(max_length=10, verbose_name="Валюта")
+    scale = models.IntegerField(verbose_name="Количество")
+    rate = models.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        verbose_name="Курс"
+    )
