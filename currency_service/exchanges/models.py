c923a32f3589f71746adfe4f00c0edcc797480af
Artuom4ik <masterskayaigr123@gmail.com>
2024-05-28 06:53:45 +0300

    Fix ExchangeRate model

--- currency_service/exchanges/models.py
+++ currency_service/exchanges/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ExchangeRate(models.Model):
     date = models.DateField(verbose_name="Дата курса")
-    currency_id = models.IntegerField(max_length=10, verbose_name="ID валюты")
+    currency_id = models.IntegerField(verbose_name="ID валюты")
     currency_name = models.CharField(max_length=10, verbose_name="Валюта")
     scale = models.IntegerField(verbose_name="Количество")
     rate = models.DecimalField(
@@ -11,3 +11,6 @@ class ExchangeRate(models.Model):
         decimal_places=4,
         verbose_name="Курс"
     )
+
+    def __str__(self):
+        return f"{self.date}, {self.currency_name}"
